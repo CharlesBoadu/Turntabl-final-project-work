95263f2f14245c1fb11b019416e3131a81e549ba
Nathanael-Azuponga <azuponganathanael@gmail.com>
2021-07-03 18:36:29 +0000

    url shortener

---
+++ app.py
@@ -0,0 +1,44 @@
+import sqlite3
+from hashids import Hashids
+from flask import Flask, render_template, request, flash, redirect, url_for
+
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'this should be a secret random string'
+
+hashids = Hashids(min_length=4, salt=app.config['SECRET_KEY'])
+
+
+@app.route('/', methods=('GET', 'POST'))
+def index():
+    conn = get_db_connection()
+
+    if request.method == 'POST':
+        url = request.form['url']
+
+        if not url:
+            flash('The URL is required!')
+            return redirect(url_for('index'))
+
+        url_data = conn.execute('INSERT INTO urls (original_url) VALUES (?)',
+                                (url,))
+        conn.commit()
+        conn.close()
+
+        url_id = url_data.lastrowid
+        hashid = hashids.encode(url_id)
+        short_url = request.host_url + hashid
+
+        return render_template('index.html', short_url=short_url)
+
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
